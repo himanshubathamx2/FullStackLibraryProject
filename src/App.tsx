d49b6191f19d8fca2299c0564c793692262c94ad
himanshubathamx2 <112793814+himanshubathamx2@users.noreply.github.com>
2023-08-01 19:04:31 +0530

    Added pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import "./App.css";
 import Navbar from "./layouts/NavbarAndFooter/Navbar";
 import Footer from "./layouts/NavbarAndFooter/Footer";
-import HomePage from "./layouts/HomePage/HomePage";
+// import HomePage from "./layouts/HomePage/HomePage";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
 
 function App() {
   return (
     <>
       <Navbar />
-      <HomePage />
+      {/* <HomePage /> */}
+      <SearchBooksPage />
       <Footer />
-      {/* check */}
     </>
   );
 }
